eb62a975ba043f8432f3a6dbc38f27a09ca70495
HEMALATHAAK005 <110532264+HEMALATHAAK005@users.noreply.github.com>
2022-11-26 14:30:25 +0530

    Add files via upload

---
+++ java.js
@@ -0,0 +1,11 @@
+
+function redpara()
+{
+    const a1=document.getElementById("p1");
+    a1.style.color="red";
+} 
+function greenpara()
+{
+    const a1=document.getElementById("p1");
+    a1.style.color="green";
+}
